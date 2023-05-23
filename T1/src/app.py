663f90e11cf03b2d408a86634d1de526b2060511
lleoc <a01643043@univie.ac.at>
2023-05-23 16:40:58 +0200

    logo and script path fixed

--- T1/src/app.py
+++ T1/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, session, flash, request, url_for
+#import results as res
 import os 
 
 app = Flask(__name__)
@@ -10,10 +11,17 @@ def init():
     return render_template('homepage.html')
 
 
-@app.route('/result', methods=['GET', 'POST'])
-def result():
+# @app.route('/result', methods=['POST'])
+# def result():
+#     data = request.get_json()
+    
+#     
+#     result = res.get_results_json(data)
+    
+#     
+#     response_data = {'result': result}
+#     return response_data
 
-    return render_template('result.html')
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
