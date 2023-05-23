460f1c58903b891f21cceef45d59a48f29ae816d
lleoc <a01643043@univie.ac.at>
2023-05-23 18:50:32 +0200

    make figures transparent

--- T1/src/results.py
+++ T1/src/results.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 def get_results_json(*drugs):
-    df = pd.read_csv('../data/CombinedDatasetConservativeTWOSIDES_critical.csv', delimiter='\t')
+    df = pd.read_csv('./data/CombinedDatasetConservativeTWOSIDES_critical.csv', delimiter='\t')
     interaction_dict = {}
     for drug in drugs:
         filtered_df = df[df['object']==drug]
@@ -16,12 +16,12 @@ def get_results_json(*drugs):
         json.dump(interaction_dict, json_file)
 
 
-# example call
-drugs = ['Efavirenz', 'Norgestimate', 'Rifampicin', 'Amprenavir', 'Fentanyl']
-get_results_json(*drugs)
+# # example call
+# drugs = ['Efavirenz', 'Norgestimate', 'Rifampicin', 'Amprenavir', 'Fentanyl']
+# get_results_json(*drugs)
 
-# data access
-with open('result.json', 'r') as json_file:
-    data = json.load(json_file)
+# # data access
+# with open('result.json', 'r') as json_file:
+#     data = json.load(json_file)
 
-print(data)
+# print(data)
